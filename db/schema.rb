8f2bbcebc27e1dfd38acc3230add4ea87be2e00b
ryo322 <135493789+ryo322@users.noreply.github.com>
2023-06-27 00:47:27 +0000

    [Add]テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_26_215041) do
+ActiveRecord::Schema.define(version: 2023_06_27_003452) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2023_06_26_215041) do
   create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "shop_name"
+    t.text "caption"
   end
 
   create_table "users", force: :cascade do |t|
